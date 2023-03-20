9b98739fd11275ef1d30add9c823531a9bc4cbf4
jontelle13 <114651407+jontelle13@users.noreply.github.com>
2023-03-20 12:46:54 +0100

    mugarri2 bukatuta

--- Milestone2/src/Milestone2.java
+++ Milestone2/src/Milestone2.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 
 import static javax.swing.ScrollPaneConstants.*;
 
 public class Milestone2 extends JFrame {
     public Milestone2() {
         JPanel mende = new JPanel();
-        mende.setPreferredSize(new Dimension(600,400));
+        mende.setPreferredSize(new Dimension(400,400));
         String [] lenguaimota={"","Python.txt","C.txt","java.txt"};
         JComboBox lenguaiak = new JComboBox(lenguaimota);
         lenguaiak.setSelectedIndex(0);
@@ -18,16 +21,58 @@ public class Milestone2 extends JFrame {
 
         JPanel eki=new JPanel();
         eki.setPreferredSize(new Dimension(600,400));
-         JTextArea testua=new JTextArea();
+        eki.setLayout(new BoxLayout(eki, BoxLayout.Y_AXIS));
+        JTextArea testua=new JTextArea();
+         testua.setPreferredSize(new Dimension(400,300));
+         testua.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(testua);
-        scrollPane.setPreferredSize(new Dimension(300,200));
+        scrollPane.setPreferredSize(new Dimension(400,300));
         scrollPane.setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_ALWAYS);
         scrollPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
+
         eki.add(scrollPane,BorderLayout.CENTER);
         add(eki, BorderLayout.EAST);
 
+        lenguaiak.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String fileName = (String) lenguaiak.getSelectedItem();
+                if (!fileName.isEmpty()) {
+                    try {
+                        File file = new File(fileName);
+                        FileReader fileReader = new FileReader(file);
+                        BufferedReader bf = new BufferedReader(fileReader);
+                        String idatzi;
+                        testua.setText("");
+                        while ((idatzi = bf.readLine()) != null) {
+                            testua.append(idatzi + "\n");
+                        }
+                        bf.close();
+                        fileReader.close();
+                    } catch (FileNotFoundException ex) {
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(Milestone2.this,
+                                "Ez da ondo irakurri");
+                    }
+                }
+            }
+        });
 
+        garbitu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                testua.setText("");
+            }
+        });
 
+        JButton itxi=new JButton("Itxi");
+        eki.add(itxi,BorderLayout.SOUTH);
+        itxi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
